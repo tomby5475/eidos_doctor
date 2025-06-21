1b4ed7724660d1d382c7092ec0a7a79a76e62b16
tomby5475 <pushkareva.nataly@gmail.com>
2025-06-21 18:32:27 +0200

    bg-crosses spans the entire screen

--- src/screens/MobileLp/sections/ImageSection/ImageSection.tsx
+++ src/screens/MobileLp/sections/ImageSection/ImageSection.tsx
@@ -24,60 +24,70 @@ const messages = [
 
 export const ImageSection = (): JSX.Element => {
   return (
-    <div className="bg-crosses w-full">
-    <div className="flex flex-col gap-6 px-4 py-8 ">
-      {messages.map((message, index) => (
-        <div
-          key={index}
-          className={`flex ${message.sender === "self" ? "flex-col items-end" : "items-end"} gap-2 w-full`}
-        >
-          {message.sender === "other" && (
-            <div
-              className="w-[30px] h-[30px] bg-cover bg-center rounded-full"
-              style={{ backgroundImage: `url(${message.avatar})` }}
-            />
-          )}
 
+<div className="bg-crosses relative left-1/2 right-1/2 w-screen -ml-[50vw] -mr-[50vw] bg-crosses">
+  <div className="flex flex-col gap-6 px-4 py-8 max-w-[393px] mx-auto w-full">
+    {messages.map((message, index) => (
+      <div
+        key={index}
+        className={`flex ${
+          message.sender === "self" ? "flex-col items-end" : "items-end"
+        } gap-2 w-full`}
+      >
+        {message.sender === "other" && (
           <div
-            className={`flex flex-col ${message.sender === "self" ? "items-start" : "items-end"} ${message.sender === "self" && index === messages.length - 1 ? "w-[165px]" : "flex-1"}`}
-          >
-            <div className="flex items-end w-full">
-              {message.sender === "other" && (
-                <div className="flex flex-col w-px h-5 items-start">
-                  <img
-                    className="mb-[-0.32px] relative w-[16.42px] h-[20.32px] ml-[-5.00px] mr-[-10.42px]"
-                    alt="Container"
-                    src="/container.svg"
-                  />
-                </div>
-              )}
+            className="w-[30px] h-[30px] bg-cover bg-center rounded-full"
+            style={{ backgroundImage: `url(${message.avatar})` }}
+          />
+        )}
 
-              <div
-                className={`px-3 py-1.5 rounded-[18px] ${
-                  message.sender === "self"
-                    ? "bg-[#0060d5] text-white"
-                    : "bg-[#262628] text-label-colordarkprimary"
-                } flex-1`}
-              >
-                <div className="font-['SF_Pro_Text-Regular',Helvetica] font-normal text-lg tracking-[-0.41px] leading-[22px]">
-                  {message.text}
-                </div>
+        <div
+          className={`flex flex-col ${
+            message.sender === "self" ? "items-start" : "items-end"
+          } ${
+            message.sender === "self" && index === messages.length - 1
+              ? "w-[165px]"
+              : "flex-1"
+          }`}
+        >
+          <div className="flex items-end w-full">
+            {message.sender === "other" && (
+              <div className="flex flex-col w-px h-5 items-start">
+                <img
+                  className="mb-[-0.32px] relative w-[16.42px] h-[20.32px] ml-[-5.00px] mr-[-10.42px]"
+                  alt="Container"
+                  src="/container.svg"
+                />
               </div>
+            )}
 
-              {message.sender === "self" && (
-                <div className="mr-[-0.90px] rotate-180 flex flex-col w-px h-5 items-start">
-                  <img
-                    className="mt-[-0.32px] -rotate-180 relative w-[16.42px] h-[20.32px] ml-[-5.00px] mr-[-10.42px]"
-                    alt="Container"
-                    src="/container-1.svg"
-                  />
-                </div>
-              )}
+            <div
+              className={`px-3 py-1.5 rounded-[18px] ${
+                message.sender === "self"
+                  ? "bg-[#0060d5] text-white"
+                  : "bg-[#262628] text-label-colordarkprimary"
+              } flex-1`}
+            >
+              <div className="font-['SF_Pro_Text-Regular',Helvetica] font-normal text-lg tracking-[-0.41px] leading-[22px]">
+                {message.text}
+              </div>
             </div>
+
+            {message.sender === "self" && (
+              <div className="mr-[-0.90px] rotate-180 flex flex-col w-px h-5 items-start">
+                <img
+                  className="mt-[-0.32px] -rotate-180 relative w-[16.42px] h-[20.32px] ml-[-5.00px] mr-[-10.42px]"
+                  alt="Container"
+                  src="/container-1.svg"
+                />
+              </div>
+            )}
           </div>
         </div>
-      ))}
-    </div>
-    </div>
+      </div>
+    ))}
+  </div>
+</div>
+
   );
 };
